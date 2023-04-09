deaab8b35403b09654bd723522e6709e9e93476d
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-04-09 13:48:04 +0200

    setup function js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,18 @@
 
 document.addEventListener('DOMContentLoaded', init);
 
+let _$canvas;
+let _ctx;
+
 function init() {
-    console.log('js connected');
+    setUpCanvas();
+}
+
+function setUpCanvas() {
+    _$canvas = document.querySelector('canvas');
+    _ctx = _$canvas.getContext('2d');
+    _$canvas.width = 1600;
+    _$canvas.height = 800;
+    // _ctx.beginPath();
+    _ctx.fillRect(0, 0, 1600, 800);
 }
