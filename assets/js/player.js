6279441a3bbfdae7f6b5072c90989e22967424d7
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-04-09 22:37:21 +0200

    added barebones move function to player

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,5 +1,33 @@
 class Player {
     constructor() {
-
+        this.x = _$canvas.height / 2;
+        this.y = _$canvas.height / 2;
+        this.radius = 7;
+        this.angle = - Math.PI / 2;
+        this.vel = 2;
     } 
+
+    draw() {
+        drawCircle('#FFFFFF', [this.x, this.y], this.radius);
+        // draw direction line
+        const directionLineLength = 20
+        drawLine('#474747', [this.x, this.y, this.x + directionLineLength * Math.cos(this.angle), this.y + directionLineLength * Math.sin(this.angle)]);
+    }
+
+    move(direction) {
+        switch (direction) {
+            case 'up':
+                this.y -= this.vel;
+                break;
+            case 'down':
+                this.y += this.vel;
+                break;
+            case 'left':
+                this.x -= this.vel;
+                break;
+            case 'right':
+                this.x += this.vel;
+                break;
+        }
+    }
 }
