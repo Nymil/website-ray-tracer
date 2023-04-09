a575273b3781f737066b9d8252e194309ac09bbb
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-04-09 15:47:46 +0200

    remove testing draw functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,4 @@ function setUpCanvas() {
     _$canvas.width = 1600;
     _$canvas.height = 800;
     drawRect('black', [0, 0, 1600, 800]);
-    drawLine('blue', [0, 0, 1600, 800]);
-    drawRect('blue', [20, 20, 600, 130]);
-    drawRect('red', [20, 150, 600, 130], 20);
-    drawRect('blue', [500, 600, 600, 130], 6);
-    drawCircle('blue', [1000, 400], 60);
 }
