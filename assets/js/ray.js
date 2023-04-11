aa53887170661e7610c801776eaa75cbd9e8e436
mattice.heyzak <mattice.heyzak@student.howest.be>
2023-04-11 14:54:57 +0200

    added ray draw function

--- assets/js/ray.js
+++ assets/js/ray.js
@@ -1,10 +1,15 @@
 class Ray {
     constructor(player, angle) {
-
+        this.startX = player.x;
+        this.startY = player.y;
+        this.endX = null;
+        this.endY = null;
+        this.angle = angle;
     }
 
     draw() {
-
+        if (this.endX === null || this.endY === null) return;
+        drawLine('white', [this.startX, this.startY, this.endX, this.endY]);
     }
 
     cast() {
